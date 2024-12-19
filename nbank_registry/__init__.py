858c86f1d183372c5b1ba0b7000dd1ae082017fb
Dan Meliza <dan@meliza.org>
2024-12-18 20:37:53 -0500

    move version to pyproject.toml, add dev suffix to test pypi uploads

--- nbank_registry/__init__.py
+++ nbank_registry/__init__.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
 # -*- mode: python -*-
-__version__ = "0.10.9"
+try:
+    from importlib.metadata import version
+    __version__ = version("django-bird-colony")
+except ImportError:
+    # For Python < 3.8
+    from importlib_metadata import version
+    __version__ = version("django-bird-colony")
+except Exception:
+    # If package is not installed (e.g. during development)
+    __version__ = "unknown"
 api_version = "1.0"
