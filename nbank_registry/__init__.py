7caf2e3418b978499100067edb63285ace3cf000
Dan Meliza <dan@meliza.org>
2024-12-18 20:40:05 -0500

    fix copy/paste error

--- nbank_registry/__init__.py
+++ nbank_registry/__init__.py
@@ -2,12 +2,8 @@
 # -*- mode: python -*-
 try:
     from importlib.metadata import version
-    __version__ = version("django-bird-colony")
+    __version__ = version("django-neurobank")
 except ImportError:
-    # For Python < 3.8
-    from importlib_metadata import version
-    __version__ = version("django-bird-colony")
-except Exception:
     # If package is not installed (e.g. during development)
     __version__ = "unknown"
 api_version = "1.0"
